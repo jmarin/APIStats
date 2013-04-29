4d99b822b4bcd9b07b8ab45d139e88dce0b87f3a
jmarin <juan.marin.otero@gmail.com>
2013-04-28 21:12:41 -0400

    Format code

--- app/models/resource/RequestHttpEndpoint.scala
+++ app/models/resource/RequestHttpEndpoint.scala
@@ -1,27 +1,18 @@
 package models.resource
 
-import akka.actor.{Actor, ActorRef, Props, ActorSystem}
-import akka.camel.{Consumer, CamelMessage}
-
-import reactivemongo.api._
-import reactivemongo.bson._
-
-import play.modules.reactivemongo._
-
-
-import play.api.libs.json
-import play.api.Play.current
+import akka.actor.Actor
+import akka.camel.{ Consumer, CamelMessage }
 
 class RequestHttpEndpoint extends Consumer with ConsumerType {
 
-	def endpointUri = globals.httpEndpoint
+  def endpointUri = globals.httpEndpoint
 
-	def receive = {
-		case CamelMessage => {
-			sender ! ("OK", self)
-		}
-		case _ =>
-			println("RECEIVED!!")
-		sender ! "OK"	
-	}
+  def receive = {
+    case CamelMessage => {
+      sender ! ("OK", self)
+    }
+    case _ =>
+      println("RECEIVED!!")
+      sender ! "OK"
+  }
 }
